4c2f8d9f2ec3d07302001205783ca2296d00ee8f
Kantandia <taylorsdsmith@gmail.com>
2019-07-01 22:36:30 -0500

    defined strorefront

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,3 +23,24 @@ connection.connect(function (err) {
     // start function 
     showProducts();
 });
+
+
+function showProducts() {
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+
+        console.log('');
+        console.log('_____________PLEASE BUY SOMETHING THEY HAVE MY CHILDREN_____________');
+        console.log('');
+
+
+        for (var i = 0; i < res.length; i++) {
+            console.log('Item ID: ' + res[i].id + '      Product: ' + res[i].product + '      Department: ' + res[i].department);
+            console.log('Price: ' + res[i].price + '      Quanity Left: ' + res[i].quanity);
+            console.log(' ');
+
+        }
+     //call function
+        start();
+    });
+}
