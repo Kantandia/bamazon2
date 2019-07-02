94e44d73236c8bb4ad0b76d31cb07c05e3ec4cc4
Kantandia <taylorsdsmith@gmail.com>
2019-07-01 22:32:08 -0500

    added connec

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,14 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var inStock = 0;
 var totalPrice = 0;
+
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+    password: "kaykay@49thieves",
+    database: "bamazon_db"
+});
